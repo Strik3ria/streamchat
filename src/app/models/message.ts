c60f385846a0978fc7f251638b6ee43cd3fa3b31
TechnicalIllusion <robert_coones@zohomail.com>
2020-09-12 22:54:46 -0500

    Added random color per user per session

--- src/app/models/message.ts
+++ src/app/models/message.ts
@@ -4,9 +4,10 @@ export class Message {
     color: string;
     isNew: boolean;
 
-    constructor(userName: string, content: string) {
+    constructor(userName: string, content: string, color: string) {
         this.userName = userName;
         this.content = content;
+        this.color = color;
         this.isNew = true;
     }
 }
