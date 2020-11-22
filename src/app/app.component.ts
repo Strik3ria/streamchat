1c5e2ed18a9c8377def952e22992adf0058e7ff0
TechnicalIllusion <robert_coones@zohomail.com>
2020-11-22 09:37:41 -0600

    feat(prettier): updated security to satisfy linting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 import { Message } from './models/message';
 import { MessageService } from './services/message.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-    title = 'streamchat';
-    messages: Message[] = [];
+export class AppComponent implements OnInit {
+  title = 'streamchat';
+  messages: Message[] = [];
 
-    constructor(private messageService: MessageService) {}
+  constructor(private messageService: MessageService) {}
 
-    ngOnInit() {
-        this.messageService.newMessage.subscribe((messages: Message[]) => this.messages = messages)
-    }
+  ngOnInit(): void {
+    this.messageService.newMessage.subscribe((messages: Message[]) => (this.messages = messages));
+  }
 }
--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -4,142 +4,158 @@ import { Message } from '../models/message';
 
 @Injectable()
 export class MessageService {
-    client: tmi.Client;
-    bottomElement: Element;
-    messages: Message[] = [];
-    newMessage = new EventEmitter<Message[]>();
-    newMessageAudio = new Audio('../assets/audio/Ding-sound-effect.mp3');
+  private client: tmi.Client;
+  private bottomElement: Element;
+  private messages: Message[] = [];
+  newMessage = new EventEmitter<Message[]>();
+  private newMessageAudio = new Audio('../assets/audio/Ding-sound-effect.mp3');
 
-    constructor() {
-        this.setup();
-    };
+  constructor() {
+    this.setup();
+  }
 
-    async setup() {
-        this.client = tmi.Client({
-            connection: {
-                reconnect: true,
-                secure: true
-            },
-            channels: [ 'billsellers5' ]
-        });
+  private async setup(): Promise<void> {
+    this.client = tmi.Client({
+      connection: {
+        reconnect: true,
+        secure: true,
+      },
+      channels: ['billsellers5'],
+    });
 
-        await this.client.connect();
+    await this.client.connect();
 
-        this.newMessages();
-        this.newSubscriber();
-        this.resubscriber();
-        this.subGift();
-        this.multiRandomGift();
-        this.giftUpgrade();
-        this.hosted();
+    this.newMessages();
+    this.newSubscriber();
+    this.resubscriber();
+    this.subGift();
+    this.multiRandomGift();
+    this.giftUpgrade();
+    this.hosted();
 
-        this.bottomElement = document.querySelector('#bottom');
-        this.newMessageAudio.load();
-    };
+    this.bottomElement = document.querySelector('#bottom');
+    this.newMessageAudio.load();
+  }
 
-    newMessages = () => {
-        this.client.on('message', (channel, tags, message, self) => {
-            let color = this.getColor(tags);
-            let userName = tags['display-name'];
+  private newMessages = () => {
+    this.client.on('message', (channel, tags, message, self) => {
+      const color = this.getColor(tags);
+      const userName = tags['display-name'];
 
-            let newMessage = new Message(userName, message, color);
-            this.addMessage(newMessage);
-        });
-    };
+      const newMessage = new Message(userName, message, color);
+      this.addMessage(newMessage);
+    });
+  };
 
-    newSubscriber = () => {
-        this.client.on('subscription', (channel, username, method, message, userstate) => {
-            let color = this.getColor(userstate);
+  private newSubscriber = () => {
+    this.client.on('subscription', (channel, username, method, message, userstate) => {
+      const color = this.getColor(userstate);
 
-            let newMessage = new Message('SUBSCRIPTION', userstate['system-msg'], color);
-            this.addMessage(newMessage);
-        });
-    };
+      const newMessage = new Message('SUBSCRIPTION', userstate['system-msg'], color);
+      this.addMessage(newMessage);
+    });
+  };
 
-    resubscriber = () => {
-        this.client.on('resub', (channel, username, months, message, userstate, methods) => {
-            let color = this.getColor(userstate);
+  private resubscriber = () => {
+    this.client.on('resub', (channel, username, months, message, userstate, methods) => {
+      const color = this.getColor(userstate);
 
-            let newMessage = new Message('RE-SUBSCRIBER', userstate['system-msg'], color);
-            this.addMessage(newMessage);
-        });
-    };
+      const newMessage = new Message('RE-SUBSCRIBER', userstate['system-msg'], color);
+      this.addMessage(newMessage);
+    });
+  };
 
-    subGift = () => {
-        this.client.on('subgift', (channel, username, months, recipient, methods, userstate) => {
-            let color = this.getColor(userstate);
+  private subGift = () => {
+    this.client.on('subgift', (channel, username, months, recipient, methods, userstate) => {
+      const color = this.getColor(userstate);
 
-            let newMessage = new Message('GIFT SUB', userstate['system-msg'], color);
-            this.addMessage(newMessage);
-        });
-    };
+      const newMessage = new Message('GIFT SUB', userstate['system-msg'], color);
+      this.addMessage(newMessage);
+    });
+  };
 
-    multiRandomGift = () => {
-        this.client.on('submysterygift', (channel, username, numOfSubs, methods, userstate) => {
-            let color = this.getColor(userstate);
+  private multiRandomGift = () => {
+    this.client.on('submysterygift', (channel, username, numOfSubs, methods, userstate) => {
+      const color = this.getColor(userstate);
 
-            let newMessage = new Message('MULTI GIFT SUB', userstate['system-msg'], color);
-            this.addMessage(newMessage);
-        });
-    };
+      const newMessage = new Message('MULTI GIFT SUB', userstate['system-msg'], color);
+      this.addMessage(newMessage);
+    });
+  };
 
-    giftUpgrade = () => {
-        this.client.on('giftpaidupgrade', (channel, username, sender, userstate) => {
-            let color = this.getColor(userstate);
+  private giftUpgrade = () => {
+    this.client.on('giftpaidupgrade', (channel, username, sender, userstate) => {
+      const color = this.getColor(userstate);
 
-            let newMessage = new Message('GIFT SUB UPGRADE', userstate['system-msg'], color);
-            this.addMessage(newMessage);
-        });
-    };
+      const newMessage = new Message('GIFT SUB UPGRADE', userstate['system-msg'], color);
+      this.addMessage(newMessage);
+    });
+  };
 
-    hosted = () => {
-        this.client.on('hosted', (channel, username, viewers, autohost) => {
-            let color = this.getColor(null);
-            let message = `${username} has hosted you with ${viewers} of their friends!`;
+  private hosted = () => {
+    this.client.on('hosted', (channel, username, viewers, autohost) => {
+      const color = this.getColor(null);
+      const message = `${username} has hosted you with ${viewers} of their friends!`;
 
-            let newMessage = new Message('HOSTED', message, color);
-            this.addMessage(newMessage);
-        });
-    };
+      const newMessage = new Message('HOSTED', message, color);
+      this.addMessage(newMessage);
+    });
+  };
 
-    addMessage = (message: Message) => {
-        let number = this.messages.push(message);
-        this.newMessage.emit(this.messages.slice());
-        this.playNewMessageSound();
-        this.scrollToBottomElement();
+  private addMessage = (message: Message) => {
+    const messageIndex = this.messages.push(message);
+    this.newMessage.emit(this.messages.slice());
+    this.playNewMessageSound();
+    this.scrollToBottomElement();
 
-        let blinking = this.getBlinkInterval(number);
-        this.timeoutBlinkAtFifteen(blinking);
+    const blinking = this.getBlinkInterval(messageIndex);
+    this.timeoutBlinkAtFifteen(blinking);
 
-        return number;
-    };
+    return messageIndex;
+  };
 
-    getColor = (state: any) => {
-        let colorList = ['purple', 'red', 'blue', 'green', 'orange', 'brown', 'cyan', 'salmon', 'royalblue', 'olive', 'springgreen', 'slategrey', 'black', 'aqua'];
-        let color = state['color'];
+  private getColor = (state: any) => {
+    const colorList = [
+      'purple',
+      'red',
+      'blue',
+      'green',
+      'orange',
+      'brown',
+      'cyan',
+      'salmon',
+      'royalblue',
+      'olive',
+      'springgreen',
+      'slategrey',
+      'black',
+      'aqua',
+    ];
+    let color = state.color;
 
-        if (color === null) {
-            color = colorList[Math.floor(Math.random() * Math.floor(13))];
-        }
+    if (color === null) {
+      color = colorList[Math.floor(Math.random() * Math.floor(13))];
+    }
 
-        return color;
-    };
+    return color;
+  };
 
-    playNewMessageSound = () => {
-        this.newMessageAudio.play();
-    };
+  private playNewMessageSound = () => {
+    this.newMessageAudio.play();
+  };
 
-    getBlinkInterval = (number: number) => setInterval(() => {
-        this.messages[number - 1].isNew = !this.messages[number - 1].isNew;
+  private getBlinkInterval = (messageIndex: number) =>
+    setInterval(() => {
+      this.messages[messageIndex - 1].isNew = !this.messages[messageIndex - 1].isNew;
     }, 1000);
 
-    timeoutBlinkAtFifteen = (interval: any) => {
-        setTimeout(() => {
-            clearInterval(interval);
-        }, 15000);
-    };
+  private timeoutBlinkAtFifteen = (interval: any) => {
+    setTimeout(() => {
+      clearInterval(interval);
+    }, 15000);
+  };
 
-    scrollToBottomElement = () => {
-        this.bottomElement.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
-    };
+  private scrollToBottomElement = () => {
+    this.bottomElement.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+  };
 }
