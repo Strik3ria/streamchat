69382973a3a976dc8a514fbbadda5600c77d497d
TechnicalIllusion <robert_coones@zohomail.com>
2021-01-03 16:29:43 -0600

    feat(message-component): add message component

---
+++ src/app/message-list/message/message.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Message } from 'src/app/models/message';
+
+@Component({
+  selector: 'app-message',
+  templateUrl: './message.component.html',
+  styleUrls: ['./message.component.css'],
+})
+export class MessageComponent implements OnInit {
+  @Input() message: Message;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
