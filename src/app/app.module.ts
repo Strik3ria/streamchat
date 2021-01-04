9eda88b7165ee4ba0a271c36fc809e88cac1cf48
TechnicalIllusion <robert_coones@zohomail.com>
2021-01-03 21:19:55 -0600

    feat(navigation-bar): add navigation bar to top of application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { MessageService } from './services/message.service';
 import { MessageListComponent } from './message-list/message-list.component';
 import { MessageComponent } from './message-list/message/message.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, MessageListComponent, MessageComponent],
+  declarations: [AppComponent, MessageListComponent, MessageComponent, NavbarComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [MessageService],
   bootstrap: [AppComponent],
